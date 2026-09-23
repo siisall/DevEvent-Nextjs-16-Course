e97e28356810b94da52972a2b7e0200f83950e34
siisall <269112503+siisall@users.noreply.github.com>
2026-09-23 17:46:58 +0700

    update konfigurasi coderabbit dan perbaikan code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { IEvent } from "@/database";
 import { events } from "@/lib/constants";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+const Page = async () => {
+
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json()
+
   return (
     <section>
       <h1 className="text-center">
@@ -17,7 +24,7 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event}/>
             </li>
